217d6998e67b6cce901ef5b64b8b81d0bf553480
GitHub Action <action@github.com>
2026-07-02 21:08:45 +0000

    [automated] Apply ESLint and Oxfmt fixes

--- src/views/LinearView.vue
+++ src/views/LinearView.vue
@@ -138,7 +138,9 @@ function dragDrop(e: DragEvent) {
           <AppBuilder v-if="showLeftBuilder" />
           <div
             v-else-if="sidebarOnLeft && activeTab"
-            v-coachmark="activeTab?.id === 'assets' ? 'assets-panel' : undefined"
+            v-coachmark="
+              activeTab?.id === 'assets' ? 'assets-panel' : undefined
+            "
             class="size-full overflow-x-hidden border-r border-border-subtle"
           >
             <ExtensionSlot :extension="activeTab" />
@@ -190,7 +192,9 @@ function dragDrop(e: DragEvent) {
           />
           <div
             v-else-if="activeTab"
-            v-coachmark="activeTab?.id === 'assets' ? 'assets-panel' : undefined"
+            v-coachmark="
+              activeTab?.id === 'assets' ? 'assets-panel' : undefined
+            "
             class="h-full overflow-x-hidden border-l border-border-subtle"
           >
             <ExtensionSlot :extension="activeTab" />
